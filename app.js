dca2033f58612747e929ee13a2310a9c5b28299b
altamz <79845676+altamz43-rgb@users.noreply.github.com>
2021-04-04 13:03:00 +0800

    dasda

---
+++ app.js
@@ -0,0 +1,17 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+
+  for (user of navLinks) {
+    user.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+    });
+  }
+};
+
+navSlide();
